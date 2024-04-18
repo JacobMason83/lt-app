10dbcb763a852db39385aa5ddb492aab16f9e183
Jacob Mason <jacob_mason@sweetwater.com>
2024-04-18 16:54:10 -0400

    inital committ

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Form, Button } from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+    <Form />
+    <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,22 @@
+export const Form = ({ action }) =>{
+    async function increment(n) {
+      return n + 1;
+    }
+    const [count, incrementFormAction] = useFormState(increment, 0);
+    return (
+      <form action={action}>
+        <button formAction={incrementFormAction}>Count: {count}</button>
+        <Button />
+      </form>
+    );
+  }
+  
+  export const Button = ()=> {
+    const { pending } = useFormStatus();
+    return (
+      <button disabled={pending} type="submit">
+        Submit
+      </button>
+    );
+  }
+ 
